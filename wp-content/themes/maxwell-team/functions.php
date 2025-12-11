7f1bf1b95734873d7840945fbedca336a799d396
gasa338 <gasa338@gmail.com>
2025-12-11 10:55:45 +0100

    ispravka za fontove

--- wp-content/themes/maxwell-team/functions.php
+++ wp-content/themes/maxwell-team/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * mma-future functions and definitions
  *
@@ -7,33 +8,35 @@
  * @package mma-future
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 /** helper test functions */
-function dd($array): void {
+function dd($array): void
+{
 	echo "<pre>";
 	print_r($array);
 	echo "</pre>";
 }
 
-if ( ! function_exists( 'tld_log' ) ) {
-	function tld_log( $entry, $mode = 'a', $file = 'tld_log' ) {
+if (! function_exists('tld_log')) {
+	function tld_log($entry, $mode = 'a', $file = 'tld_log')
+	{
 		// Get WordPress uploads directory.
 		$upload_dir = wp_upload_dir();
 
 		$upload_dir = $upload_dir['basedir'];
 		$upload_dir = dirname(__FILE__);
 		// If the entry is array, json_encode.
-		if ( is_array( $entry ) ) {
-			$entry = json_encode( $entry );
+		if (is_array($entry)) {
+			$entry = json_encode($entry);
 		}
 		// Write the log file.
 		$file  = $upload_dir . '/' . $file . '.log';
-		$file  = fopen( $file, $mode );
-		$bytes = fwrite( $file, current_time( 'mysql' ) . "::" . $entry . "\n" );
-		fclose( $file );
+		$file  = fopen($file, $mode);
+		$bytes = fwrite($file, current_time('mysql') . "::" . $entry . "\n");
+		fclose($file);
 		return $bytes;
 	}
 }
@@ -45,17 +48,18 @@ if ( ! function_exists( 'tld_log' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function mma_future_setup() {
+function mma_future_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on mma-future, use a find and replace
 		* to change 'mma-future' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'mma-future', get_template_directory() . '/languages' );
+	load_theme_textdomain('mma-future', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -63,19 +67,19 @@ function mma_future_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'primary' => esc_html__( 'Primary', 'mma-future' ),
+			'primary' => esc_html__('Primary', 'mma-future'),
 		)
 	);
 
@@ -109,7 +113,7 @@ function mma_future_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -126,7 +130,7 @@ function mma_future_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'mma_future_setup' );
+add_action('after_setup_theme', 'mma_future_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -135,22 +139,24 @@ add_action( 'after_setup_theme', 'mma_future_setup' );
  *
  * @global int $content_width
  */
-function mma_future_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'mma_future_content_width', 640 );
+function mma_future_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('mma_future_content_width', 640);
 }
-add_action( 'after_setup_theme', 'mma_future_content_width', 0 );
+add_action('after_setup_theme', 'mma_future_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function mma_future_widgets_init() {
+function mma_future_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'mma-future' ),
+			'name'          => esc_html__('Sidebar', 'mma-future'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'mma-future' ),
+			'description'   => esc_html__('Add widgets here.', 'mma-future'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -158,52 +164,55 @@ function mma_future_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'mma_future_widgets_init' );
+add_action('widgets_init', 'mma_future_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function mma_future_scripts() {
+function mma_future_scripts()
+{
 	/** ==============================            custom styles and scripts            ============================== */
 	/**  */
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/dist/css/output.css' );
-	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/dist/js/main.js', array(), _S_VERSION, true );
+	wp_enqueue_style('main', get_template_directory_uri() . '/assets/dist/css/output.css');
+	wp_enqueue_script('main', get_template_directory_uri() . '/assets/dist/js/main.js', array(), _S_VERSION, true);
 	// wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/dist/fontawesome/css/all.min.css?v=6.5.2' );
 
 
-    /** ================================================================================================================ */
+	/** ================================================================================================================ */
 
 
 	/** ==============================            default styles and scripts            ============================== */
 
-	wp_enqueue_style( 'mma-future-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'mma-future-style', 'rtl', 'replace' );
+	wp_enqueue_style('mma-future-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('mma-future-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'mma-future-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('mma-future-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 
-	wp_dequeue_style( 'wp-block-library' );
-	wp_dequeue_style( 'wp-block-library-theme' );
-	wp_dequeue_style( 'wc-blocks-style' ); // Ako koristite WooCommerce
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	wp_dequeue_style('wc-blocks-style'); // Ako koristite WooCommerce
+	wp_dequeue_style('global-styles');
 }
-add_action( 'wp_enqueue_scripts', 'mma_future_scripts' );
+add_action('wp_enqueue_scripts', 'mma_future_scripts');
 
 
 /**
  * Enqueue admin scripts
  */
-function mma_future_admin_scripts($hook) {
+function mma_future_admin_scripts($hook)
+{
 	global $screen_options;
-	if ( 'post.php' === $hook ) {
-		wp_enqueue_style( 'mma-main', get_template_directory_uri() . '/assets/dist/css/output.css' );
-		wp_enqueue_script( 'mma-main', get_template_directory_uri() . '/assets/dist/js/main.js', array(), _S_VERSION, true );
+	if ('post.php' === $hook) {
+		wp_enqueue_style('mma-main', get_template_directory_uri() . '/assets/dist/css/output.css');
+		wp_enqueue_script('mma-main', get_template_directory_uri() . '/assets/dist/js/main.js', array(), _S_VERSION, true);
 		// wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/dist/fontawesome/css/all.min.css' );
 	}
 }
-add_action( 'admin_enqueue_scripts', 'mma_future_admin_scripts' );
+add_action('admin_enqueue_scripts', 'mma_future_admin_scripts');
 
 
 /**
@@ -244,7 +253,6 @@ require get_template_directory() . '/inc/components.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
