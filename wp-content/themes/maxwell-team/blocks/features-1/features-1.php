31d92a75bdfcafea98fc53ddcf873fdd60a931d8
gasa338 <gasa338@gmail.com>
2025-12-13 14:01:15 +0100

    ispravka za features 1 blok

--- wp-content/themes/maxwell-team/blocks/features-1/features-1.php
+++ wp-content/themes/maxwell-team/blocks/features-1/features-1.php
@@ -32,11 +32,11 @@ $data = get_field('features_1');
                         <div class="p-6">
                             <!-- <div class="flex items-end justify-between mb-4"> -->
                             <?php if ($item['duration']) : ?>
-                                <div class="flex items-center gap-1 text-secondary font-bold mb-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4 text-primary">
+                                <div class="flex items-center gap-1 mb-2 text-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4">
                                         <path fill="currentColor" d="M256 0a256 256 0 1 1 0 512 256 256 0 1 1 0-512zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"></path>
                                     </svg>
-                                    <span><?php echo $item['duration']; ?></span>
+                                    <span class="font-semibold"><?php echo $item['duration']; ?></span>
                                 </div>
                             <?php endif; ?>
                             <!-- </div> -->
@@ -44,7 +44,7 @@ $data = get_field('features_1');
                             <?php if (!empty($item['title'])): ?>
                                 <h3 class="text-primary text-xl font-semibold mb-2"><?php echo $item['title']; ?></h3>
                             <?php endif; ?>
-                            <?php if (!empty($item['title'])): ?>
+                            <?php if (!empty($item['description'])): ?>
                                 <p><?php echo $item['description']; ?></p>
                             <?php endif; ?>
                             <?php if (! empty($item['places'])): ?>
