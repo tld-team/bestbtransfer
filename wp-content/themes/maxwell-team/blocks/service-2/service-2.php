513dc43d869b6231fbc8f094859947a1b9f1669f
gasa338 <gasa338@gmail.com>
2025-12-13 13:10:22 +0100

    izmena na service 2

--- wp-content/themes/maxwell-team/blocks/service-2/service-2.php
+++ wp-content/themes/maxwell-team/blocks/service-2/service-2.php
@@ -12,7 +12,7 @@ $right = $data['right'];
 </style>
 <section class="service-2 py-8 sm:py-12 lg:py-18 service-2-<?php echo esc_attr($blocks_id); ?> <?php echo esc_attr($blocks_class); ?>" id="<?php echo esc_attr($anchor); ?>">
     <div class="container mx-auto px-4">
-        <div class="text-center mb-16">
+        <div class="text-center mb-12">
             <?php if (!empty($data['top_title'])) { ?>
                 <span class="maxwell-top-title"><?php echo $data['top_title']; ?></span>
             <?php } ?>
@@ -24,7 +24,7 @@ $right = $data['right'];
             <?php } ?>
         </div>
 
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <?php if (!empty($data['left'])) : ?>
                 <div class="grid grid-cols-2 gap-6">
                     <?php foreach ($data['left'] as $item) : ?>
@@ -35,10 +35,10 @@ $right = $data['right'];
                                 src="<?php echo $item['image']['url']; ?>"
                                 alt="<?php echo $item['image']['alt']; ?>"
                                 class="absolute inset-0 w-full h-full object-cover group-hover:scale-110 transition-transform duration-500">
-                            <div class="text-content relative z-10 p-6 text-white flex flex-col justify-between h-full transition-opacity duration-300">
+                            <div class="text-content relative z-10 px-2 py-6 md:px-6 text-white flex flex-col justify-between h-full transition-opacity duration-300">
                                 <div class="inline-flex item-center">
                                     <?php if (!empty($item['icon']['subtype'] == 'svg+xml')) {
-                                        echo maxwell_render_svg($item['icon']['url'], 'w-6 h-6 mb-4 mr-2 text-white/90');
+                                        echo maxwell_render_svg($item['icon']['url'], 'w-8 h-8 md:w-6 md:h-6 mr-2 text-white/90');
                                     } ?>
                                     <h3 class="text-xl text-white font-bold mb-2"><?php echo $item['title']; ?></h3>
                                 </div>
@@ -55,7 +55,7 @@ $right = $data['right'];
             <!-- Right Column - Details -->
             <div class="flex flex-col justify-center">
                 <?php if (!empty($right['title'])) : ?>
-                    <h3 class="h3-responsive mt-2 mb-6"><?php echo $right['title']; ?></h3>
+                    <h3 class="text-xl md:text-3xl mb-6"><?php echo $right['title']; ?></h3>
                 <?php endif; ?>
                 <?php if (!empty($right['description'])) : ?>
                     <div class="mb-8 content-list-link"><?php echo apply_filters('the_content', $right['description']); ?></div>
@@ -68,12 +68,14 @@ $right = $data['right'];
                                 <?php if (!empty($item['icon'])) : ?>
                                     <div class="flex-shrink-0 w-14 h-14 bg-button/10 rounded-xl flex items-center justify-center mr-4">
                                         <?php if (!empty($item['icon']['subtype'] == 'svg+xml')) {
-                                            echo maxwell_render_svg($item['icon']['url'], 'w-6 h-6 text-button');
+                                            echo maxwell_render_svg($item['icon']['url'], 'w-6 h-6 text-primary');
                                         } ?>
                                     </div>
                                 <?php endif; ?>
                                 <div>
-                                    <h4 class="h4-responsive mt-2 mb-1 font-semibold"><?php echo $item['title']; ?></h4>
+                                    <?php if (!empty($item['title'])) : ?>
+                                        <h4 class="h4-responsive mt-2 mb-1"><?php echo $item['title']; ?></h4>
+                                    <?php endif; ?>
                                     <?php if (!empty($item['description'])) : ?>
                                         <div class="content-list-link"><?php echo apply_filters('the_content', $item['description']); ?></div>
                                     <?php endif; ?>
@@ -83,9 +85,9 @@ $right = $data['right'];
                     <?php endif; ?>
                 </div>
 
-                <div class="mt-8">
-                    <?php if (!empty($right['button'])) : ?>
-                        <?php link_1($right['button']); ?>
+                <div class="inline-block max-w-max mt-8">
+                    <?php if (!empty($right['link'])) : ?>
+                        <?php link_1($right['link']); ?>
                     <?php endif; ?>
                 </div>
             </div>
--- wp-content/themes/maxwell-team/inc/components.php
+++ wp-content/themes/maxwell-team/inc/components.php
@@ -14,7 +14,7 @@
 function link_1($link, $class = '')
 {
 ?>
-    <a href="<?php echo esc_url($link['url']); ?>" <?php echo $link['target'] ? 'target="_blank"' : ''; ?> class="border-2 border-button text-white bg-button px-6 py-2 rounded-xl hover:bg-secondary/90 shadow-gold hower:text-white hover:shadow-lg hover:-translate-y-0.5 active:scale-95 transition-all duration-300 flex items-center justify-center <?php echo esc_attr($class); ?>"><?php echo esc_html($link['title']); ?>
+    <a href="<?php echo esc_url($link['url']); ?>" <?php echo $link['target'] ? 'target="_blank"' : ''; ?> class="border-2 border-button text-white bg-button px-6 py-2 rounded-xl hover:bg-secondary/90 shadow-gold hover:!text-white hover:shadow-lg hover:-translate-y-0.5 active:scale-95 transition-all duration-300 flex items-center justify-center <?php echo esc_attr($class); ?>"><?php echo esc_html($link['title']); ?>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
             class="w-4 h-3 ml-2 transition-transform duration-200 ease-in-out group-hover:translate-x-1">
             <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-105.4 105.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
